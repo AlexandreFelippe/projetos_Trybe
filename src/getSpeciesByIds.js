54a5531701fd55b62184361ae2cbe41e8eee5daf
AlexandreFelippe <alemfelippe@gmail.com>
2023-06-13 14:36:18 -0300

    Iniciando os exercícios

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const a = species.filter((specie) => ids.includes(specie.id)) ? residents[name] : [];
+  console.log(ids);
 };
-
+getSpeciesByIds();
 module.exports = getSpeciesByIds;
