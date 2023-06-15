626655528606b2a7d3a14616d8179cc2b6890003
AlexandreFelippe <alemfelippe@gmail.com>
2023-06-15 15:17:49 -0300

    Resolve exercício 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    const obj = {};
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
+  if (animal.sex === 'female' || animal.sex === 'male') {
+    const countAnimalSex = species.filter((specie) => specie.name === animal.species);
+    return countAnimalSex[0].residents.filter((specie) => specie.sex === animal.sex).length;
+  }
+  const selectedSpecies = species.filter((specie) => specie.name === animal.species);
+  return selectedSpecies[0].residents.length;
 };
 
 module.exports = countAnimals;
