98d7a9afbf81fbdddfba6ce3cf913e1238475f45
AlexandreFelippe <alemfelippe@gmail.com>
2023-06-16 11:06:38 -0300

    Resolve exercício 08

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const { adult: adultPrice, senior: seniorPrice, child: childPrice } = prices;
+  const sum = (adult * adultPrice + senior * seniorPrice + child * childPrice);
+  return sum;
 };
 
 module.exports = { calculateEntry, countEntrants };
